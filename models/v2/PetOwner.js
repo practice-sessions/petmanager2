93ac6434c177d81540265aa73d68f3870d1b5a71
hixsav <hi.savis@gmail.com>
2019-06-27 16:08:24 +0100

    Update models' directory structure, add petowner model in v2 folder

---
+++ models/v2/PetOwner.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema - "PetOwner" == pet owner 
+const PetOwnerSchema = new Schema({
+	name: {
+		type: String,
+	//	required: 'A name is needed for the pet owner or client'
+	},
+	contactnumber: {
+		type: Number,
+	// required: 'Pet owners contact number is required'
+	},
+	address: [
+		{
+			house: {
+        type: String
+      },
+      street: {
+        type: String
+      }, 
+      street2: {
+        type: String
+      },
+      postcode: {
+        type: String
+      },
+      city: {
+        type: String
+      },
+    }
+  ],
+	pets: [
+		{
+			// Array allows possibility of more than one pet 
+			type: Schema.Types.ObjectId,
+			ref: 'pet'
+		}
+	],
+	email: {
+		type: String
+	},
+	password: {
+		type: String
+	},
+	avatar: {
+		type: String
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
+}); 
+ 
+module.exports = PetOwner = mongoose.model('petowner', PetOwnerSchema); 
